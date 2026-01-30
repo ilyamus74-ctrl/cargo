b6836cabe04f7d5a075ce02e675848baaacae3f6
Illia <ilya@example.local>
2026-01-30 13:56:50 +0200

    sync local -> remote

--- templates_c/d3b2cc55e44ccac55c216a4345cae548aa3ba653_0.file_cells_NA_API_tools_management.html.php
+++ templates_c/d3b2cc55e44ccac55c216a4345cae548aa3ba653_0.file_cells_NA_API_tools_management.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.3.1, created on 2026-01-30 11:38:15
+/* Smarty version 5.3.1, created on 2026-01-30 11:54:06
   from 'file:cells_NA_API_tools_management.html' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.3.1',
-  'unifunc' => 'content_697c9827963761_06295812',
+  'unifunc' => 'content_697c9bdecb6864_21712222',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'd3b2cc55e44ccac55c216a4345cae548aa3ba653' => 
     array (
       0 => 'cells_NA_API_tools_management.html',
-      1 => 1769773084,
+      1 => 1769773720,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_697c9827963761_06295812 (\Smarty\Template $_smarty_tpl) {
+function content_697c9bdecb6864_21712222 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/home/cells/web/templates';
 ?>    <div class="pagetitle">
       <h1>Управление инструментами</h1>
@@ -259,6 +259,14 @@ $_smarty_current_dir = '/home/cells/web/templates';
               },
               "wait_for_confirm": {
                 "mode": "none",
+                "qr": {
+                  "action": "web_callback",
+                  "callback": "setToolsUserFromQR"
+                },
+                "barcode": {
+                  "action": "web_callback",
+                  "callback": "setToolsUserFromQR"
+                },
                 "on_action": {
                   "scan":    [ { "op": "set_step", "to": "scan_user_qr" }, { "op": "open_scanner", "mode": "qr" } ],
                   "confirm": [ { "op": "web", "name": "triggerToolsManagementSave" } ],
@@ -294,6 +302,14 @@ $_smarty_current_dir = '/home/cells/web/templates';
               },
               "wait_for_confirm": {
                 "mode": "none",
+                "qr": {
+                  "action": "web_callback",
+                  "callback": "setToolsCellFromQR"
+                },
+                "barcode": {
+                  "action": "web_callback",
+                  "callback": "setToolsCellFromQR"
+                },
                 "on_action": {
                   "scan":    [ { "op": "set_step", "to": "scan_cell_qr" }, { "op": "open_scanner", "mode": "qr" } ],
                   "confirm": [ { "op": "web", "name": "triggerToolsManagementSave" } ],
