37e13ab54e8be396fdfef3c5b9351d77b9ef5b59
Illia <ilya@example.local>
2026-03-17 11:52:48 +0200

    sync local -> remote

--- templates_c/4e0cfb81384357625659d5eca445a63482fe7323_0.file_cells_NA_API_connector_operations_modal.html.php
+++ templates_c/4e0cfb81384357625659d5eca445a63482fe7323_0.file_cells_NA_API_connector_operations_modal.html.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 5.3.1, created on 2026-03-17 09:47:38
+/* Smarty version 5.3.1, created on 2026-03-17 09:50:38
   from 'file:cells_NA_API_connector_operations_modal.html' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.3.1',
-  'unifunc' => 'content_69b9233ac959a8_25600715',
+  'unifunc' => 'content_69b923ee623ab9_85801340',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_69b9233ac959a8_25600715 (\Smarty\Template $_smarty_tpl) {
+function content_69b923ee623ab9_85801340 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/home/cells/web/templates';
 ?><section class="section">
   <div class="row">
