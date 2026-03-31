31d69e7ff74761998b676b5df9cbff69a4bd8f72
Illia <ilya@example.local>
2026-03-31 11:39:15 +0300

    sync local -> remote

--- templates_c/4e0cfb81384357625659d5eca445a63482fe7323_0.file_cells_NA_API_connector_operations_modal.html.php
+++ templates_c/4e0cfb81384357625659d5eca445a63482fe7323_0.file_cells_NA_API_connector_operations_modal.html.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 5.3.1, created on 2026-03-31 05:42:38
+/* Smarty version 5.3.1, created on 2026-03-31 08:38:10
   from 'file:cells_NA_API_connector_operations_modal.html' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.3.1',
-  'unifunc' => 'content_69cb5ecee9cd53_11812287',
+  'unifunc' => 'content_69cb87f22bc248_12725199',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_69cb5ecee9cd53_11812287 (\Smarty\Template $_smarty_tpl) {
+function content_69cb87f22bc248_12725199 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/home/cells/web/templates';
 ?><section class="section">
   <style>
