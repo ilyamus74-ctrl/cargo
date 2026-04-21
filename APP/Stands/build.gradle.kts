b8394587474b6a365b23fff9359c09983ac0cfb2
Illia <ilya@example.local>
2026-04-21 15:51:47 +0300

    sync local -> remote

--- APP/Stands/build.gradle.kts
+++ APP/Stands/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.stand"
+    namespace = "com.example.ocrscannertest"
     compileSdk {
         version = release(36)
     }
 
     defaultConfig {
-        applicationId = "com.example.stand"
+        applicationId = "com.example.ocrscannertest"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -42,16 +42,26 @@ android {
 }
 
 dependencies {
-
-    // Compose
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.material3:material3:1.3.0")
-
-    // USB serial
-    implementation("com.github.mik3y:usb-serial-for-android:v3.10.0")
-    implementation("com.google.zxing:core:3.5.3")
-
-    // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
 }
